5319b9681531aed3ef4ae3e1cda51ed9bc0cc1b7
caio pablo  veloso cutrim <caio.cutrim@somospi.com.br>
2021-02-03 17:27:13 -0300

    move to new dir structures and add new simple challenge from codewars

---
+++ challenges/strings-ends-with.js
@@ -0,0 +1,15 @@
+
+// Complete the solution so that it returns true if the first argument(string) 
+// passed in ends with the 2nd argument (also a string).
+const assert = require('assert');
+
+
+assert.strictEqual(solution('abc', 'bc'), true) // returns true
+assert.strictEqual(solution('abc', ''), true) // returns true
+assert.strictEqual(solution('abc', 'd'), false) // returns false
+assert.strictEqual(solution('cai', 'i'), true)
+assert.strictEqual(solution('ninja', 'ja'), true)
+function solution(str, ending) {
+  let str1 = str.substr(str.length - ending.length)
+  return str1 === ending
+}
