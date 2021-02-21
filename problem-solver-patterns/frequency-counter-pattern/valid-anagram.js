24ce761a16e337e913ce0b1471c938d7ce85b44b
caio pablo  veloso cutrim <caio.cutrim@somospi.com.br>
2021-02-21 11:58:07 -0300

    refactoring valid-anagram solution

--- problem-solver-patterns/frequency-counter-pattern/valid-anagram.js
+++ problem-solver-patterns/frequency-counter-pattern/valid-anagram.js
@@ -1,35 +1,29 @@
 
-validAnagram('ana', 'ana')// true
-validAnagram('anagram', 'nagaram')// true
-validAnagram('', '') // true
-validAnagram('querty', 'qeywrt') // true
-validAnagram('car', 'rat') // false
 
-function validAnagram (str1, str2) {
-  if (str1.length === 0 && str2.length === 0) {
-    console.log(true)
-    return true
-  }
+function validAnagram (first, second) {
+  if (first.length !== second.length) return false
 
-  if (str1.length !== str2.length) {
-    return false
-  }
-  const str1ObjCounter = {}
-  const str2ObjCounter = {}
-  for (let chars of str1) {
-    str1ObjCounter[chars] = (str1ObjCounter[chars] || 0) + 1
-  }
-  for (let chars of str2) {
-    str2ObjCounter[chars] = (str2ObjCounter[chars] || 0) + 1
+  const lookup = {}
+
+  for(let i = 0; i < first.length; i++) {
+    let letter = first[i]
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
   }
 
-  for (let key in str1ObjCounter) {
-    if (str1ObjCounter[key] === str2ObjCounter[key]) {
-      console.log(true)
-      return true
-    } else {
-      console.log(false)
+  for(let i = 0; i < second.length; i++) {
+    let letter = second[i]
+    if(!lookup[letter]) {
       return false
+    } else {
+      lookup[letter] - 1
     }
   }
+
+  return true
 }
+
+console.log(validAnagram('ana', 'ana'))// true
+console.log(validAnagram('anagram', 'nagaram'))// true
+console.log(validAnagram('', '')) // true
+console.log(validAnagram('qwerty', 'ytrewq')) // true
+console.log(validAnagram('car', 'rat')) // false
